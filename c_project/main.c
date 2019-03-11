5b35b38608cecdfae08ecc3f1c59c0e1ef2f9f36
Dmitriy Volkov <mcslash@yandex.ru>
2019-03-11 16:50:07 +0300

    Не оптимизированная, но рабочая версия третьей задачи.

--- c_project/main.c
+++ c_project/main.c
@@ -1,94 +1,167 @@
-﻿#include <stdio.h>
-#include <locale.h>
-#include <limits.h>
-#include <math.h>
-#include <errno.h>
-#include <string.h>
-#include "sorters.c"
-#include "finders.c"
+﻿//3. ***Требуется обойти конём шахматную доску размером N Ч M, пройдя через все поля доски
+//по одному разу.Здесь алгоритм решения такой же, как и в задаче о 8 ферзях.Разница только в
+//проверке положения коня.
+// Дмитрий Волков
 
+
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#define T char
 #define _ separator();
-#define MaxN 100
+
+typedef enum { false, true } bool;
 
 void separator() {
+	printf("\n");
 	for (int i = 0; i < 80; i++)
 		printf("-");
-	puts("\n");
-}
-
-void print(int N, int *a) {
-	for (int i = 0; i < N; i++)
-		printf("%4i", a[i]);
 	printf("\n");
 }
 
-void sol1(int len, int *a) {
-	/*1.  Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций
-		  сравнения оптимизированной и неоптимизированной программы. Написать функции
-		  сортировки, которые возвращают количество операций.
-		Дмитрий Волков
-	*/
-	int wkArr[MaxN];
-	memcpy(wkArr, a, sizeof(wkArr));
+// Опишем структуру узла списка
+struct TNode
+{
+	T value;
+	struct TNode *next;
+};
+typedef struct TNode Node;
 
-	puts("Bubble sort");
-	puts("Исходный массив");
-	print(len, wkArr);
-	printf("Не оптимизированная сортировка пузырьком. Количество операций: %i\n", bubbleSort(len, wkArr));
-	print(len, wkArr);
+struct TStack
+{
+	Node *head;
+	int size;
+	int maxSize;
+};
+typedef struct TStack Stack;
+Stack opened;
+Stack closed;
+Stack stack;
 
-	memcpy(wkArr, a, sizeof(wkArr)); // Исходный массив
-	printf("Оптимизированная сортировка пузырьком. Количество операций: %i\n", optBubbleSort(len, wkArr));
-	print(len, wkArr);
+void push(Stack *stk, T value)
+{
+	if (stk->size >= stk->maxSize) {
+		printf("Error stack size");
+		return;
+	}
 
+	//2.Добавить в программу «Реализация стека на основе односвязного списка»
+	// проверку на выделение памяти. Если память не выделяется, то должно
+	// выводиться соответствующее сообщение. Постарайтесь создать ситуацию,
+	// когда память не будет выделяться(добавлением большого количества данных)
+	// Дмитрий Волков
+
+	Node *tmp;
+	if ((tmp = (Node*)malloc(sizeof(Node))) != 0) {
+		tmp->value = value;
+		tmp->next = stk->head;
+		stk->head = tmp;
+		stk->size++;
+	} else {
+		printf("Insufficient memory available\n");
+	}
 }
 
-void sol2(int len, int *a) {
-	/*2. *Реализовать шейкерную сортировку*/
-	puts("Shaker");
-	int wkArr[MaxN];
-	puts("Исходный массив");
-	memcpy(wkArr, a, sizeof(wkArr));
-	print(len, wkArr);
-	printf("Шейкерная сортировка. Количество операций: %i\n", shakeIt(len, wkArr));
-	print(len, wkArr);
-
+T pop(Stack *stk) {
+	if (stk->size == 0)
+	{
+		printf("Stack is empty");
+		return '\0';
+	}
+	// Временный указатель
+	Node* next = NULL;
+	// Значение «наверху» списка
+	T value;
+	value = stk->head->value;
+	next = stk->head;
+	stk->head = stk->head->next;
+	// Запись, на которую указывала «голова», удаляем, освобождая память
+	free(next);
+	// Возвращаем значение, которое было в «голове»
+	stk->size--;
+	return value;
 }
 
-void sol3(int len, int *a) {
-	/* 3. Реализовать бинарный алгоритм поиска в виде функции, которой передаётся отсортированный
-		  массив. Функция возвращает индекс найденного элемента или –1, если элемент не найден.*/	puts("Binary search");
-	int wkArr[MaxN];
-	memcpy(wkArr, a, sizeof(wkArr));	optBubbleSort(len, wkArr);	puts("Исходный массив");	print(len, wkArr);	int findFor = 47;	printf("idx of %i is %i", findFor, binary(findFor, len, wkArr));
+void PrintStack(Stack *stk)
+{
+	Node *current = stk->head;
+	while (current != NULL)
+	{
+		printf("%c ", current->value);
+		current = current->next;
+	}
 }
 
-int main(int argc, char *argv[]) {
-	setlocale(LC_ALL, "Rus");
-	int srcArray[MaxN];
-	int arrLen;
-	errno_t err;
-	FILE *in;
 
-	if ((err = fopen_s(&in, ".\\data.txt", "r")) == 0) {
-		fscanf_s(in, "%i", &arrLen);
-		for (int i = 0; i < arrLen; i++) {
-			fscanf_s(in, "%i", &srcArray[i]);
+void sol1() {
+	//2.Добавить в программу «Реализация стека на основе односвязного списка»
+	// проверку на выделение памяти. Если память не выделяется, то должно
+	// выводиться соответствующее сообщение. Постарайтесь создать ситуацию,
+	// когда память не будет выделяться(добавлением большого количества данных)
+	// Дмитрий Волков
+	stack.maxSize = 100;
+	stack.head = NULL;
+	push(&stack, 'a');
+	push(&stack, 'b');
+	push(&stack, 'c');
+	push(&stack, 'd');
+	push(&stack, 'e');
+	push(&stack, 'f');
+	PrintStack(&stack);
+}
+
+
+void sol2() {
+	// Написать программу, которая определяет, является ли введённая скобочная
+	// последовательность правильной.
+	// Примеры правильных скобочных выражений
+	// (), ([])(), {}(), ([{}]),
+	// неправильных
+	// )(, ())({ ), (, ]) }), ([(]),
+	// для скобок [, (, { .
+	// Например: (2 + (2 * 2)) или [2 / {5 * (4 + 7)}].
+	// выражение:
+	const int s_size = 100;
+	char *string = "(2 + (2 * 2))";
+	int length = 0;
+	for (int i = 0; i < string[i] != '\0'; i++)
+		length++;
+	char paired = false;
+	opened.maxSize = s_size;
+	opened.head = NULL;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (string[i] == '[' || string[i] == '(' || string[i] == '{') {
+			push(&opened, string[i]);
 		}
-		fclose(in);
-
-		// Первое задание
-		sol1(arrLen, srcArray);
-		_
-		// Второе задание
-		sol2(arrLen, srcArray);
-		_
-		// Третье задание
-		sol3(arrLen, srcArray);
-
+		else if (string[i] == ']' || string[i] == ')' || string[i] == '}') {
+			char pair = pop(&opened);
+			switch (string[i])
+			{
+			case ']':
+				if (pair == '[') paired = true;
+				break;
+			case ')':
+				if (pair == '(') paired = true;
+				break;
+			case '}':
+				if (pair == '{') paired = true;
+				break;
+			default:
+				break;
+			}
+			if (!paired) printf("Не все скобки парные! '%с' '%с'\n", string[i], pair);
+		}
 	}
-	else {
-		fprintf(stderr, "cannot open file");
-	}
+}
 
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	sol1();
+	_
+	sol2();
 	return 0;
 }
